c1dc84d77d66d7840d116e0619e9610476480270
jude-daniel1 <judedaniel360@gmail.com>
2022-08-09 11:26:05 +0100

    style the story section and install tailwind scrollbar

--- components/Story.jsx
+++ components/Story.jsx
@@ -2,9 +2,13 @@ import React from "react";
 
 export default function Story({ username, img }) {
   return (
-    <div>
-      <img src={img} alt={username} />
-      <p>{username}</p>
+    <div className="truncate min-w-max">
+      <img
+        src={img}
+        alt={username}
+        className="h-14 rounded-full p-[1.5px] border-red-500 border-2 cursor-pointer hover:scale-110 transition-transform duration-200 ease-in-out"
+      />
+      <p className="text-xs w-14 truncate">{username}</p>
     </div>
   );
 }
